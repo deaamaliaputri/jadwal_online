1bad43c6f1becd64215906f824134e61d5cbb110
DeaAmalia <pamalia575@gmail.com>
2017-05-04 09:11:23 +0700

    Daftar

--- database/seeds/daftars_table_seeder.php
+++ database/seeds/daftars_table_seeder.php
@@ -14,12 +14,12 @@ class daftars_table_seeder extends Seeder
  DB::table('daftars')->truncate();
 
         $daftars = [
-            ['id' => 1, 'name' => 'dea', 'email' => 'dea@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345','status' => 'ada', 'level' => '12345', 'nip' => '11167', 'created_at' => \Carbon\Carbon::now()],
-            ['id' => 2, 'name' => 'amalia', 'email' => 'amalia@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => 'ada', 'level' => '12346', 'nip' => '435', 'created_at' => \Carbon\Carbon::now()],
-            ['id' => 3, 'name' => 'putri', 'email' => 'putri@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => 'ada', 'level' => '12347', 'nip' => '243556', 'created_at' => \Carbon\Carbon::now()],
-            ['id' => 4, 'name' => 'cintya', 'email' => 'cintya@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => 'ada', 'level' => '12348', 'nip' => '2323', 'created_at' => \Carbon\Carbon::now()],
-            ['id' => 5, 'name' => 'dewi', 'email' => 'dewi@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => 'ada', 'level' => '12349', 'nip' => '5656', 'created_at' => \Carbon\Carbon::now()],
-            ['id' => 6, 'name' => 'nur', 'email' => 'nur@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => 'ada', 'level' => '12350', 'nip' => '2324', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 1, 'name' => 'dea', 'email' => 'dea@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345','status' => '0', 'level' => '0', 'nip' => '11167', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 2, 'name' => 'amalia', 'email' => 'amalia@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => '1', 'level' => '1', 'nip' => '435', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 3, 'name' => 'putri', 'email' => 'putri@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => '0', 'level' => '0', 'nip' => '243556', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 4, 'name' => 'cintya', 'email' => 'cintya@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => '1', 'level' => '1', 'nip' => '2323', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 5, 'name' => 'dewi', 'email' => 'dewi@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => '0', 'level' => '0', 'nip' => '5656', 'created_at' => \Carbon\Carbon::now()],
+            ['id' => 6, 'name' => 'nur', 'email' => 'nur@gmail.com', 'password' => bcrypt('12345678'), 'phone' => '08912345', 'status' => '1', 'level' => '1', 'nip' => '2324', 'created_at' => \Carbon\Carbon::now()],
         ];
 
         // insert batch
